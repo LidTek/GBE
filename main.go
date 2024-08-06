7ade2672b4e9a7c55cadac1d8a1f2ba32bf3accb
kettek <kts@kettek.net>
2024-08-05 18:41:19 -0700

    Add untested user/room logic

--- main.go
+++ main.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/sessions"
+)
+
+var store = sessions.NewCookieStore([]byte("don't care"))
+
+// track adds a session for the given user so we can identify who they are.
+func track(w http.ResponseWriter, r *http.Request) (*sessions.Session, error) {
+	session, err := store.Get(r, "session")
+	if err != nil {
+		return nil, err
+	}
+
+	if session.IsNew {
+		session.Values["ID"] = uuid.NewString()
+		if err := session.Save(r, w); err != nil {
+			return nil, err
+		}
+	}
+	return session, nil
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		session, err := track(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("Hello, " + session.Values["ID"].(string)))
+	})
+
+	http.HandleFunc("/rooms/{rid}", func(w http.ResponseWriter, r *http.Request) {
+		session, err := track(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		switch r.Method {
+		case "POST": // POST attempts to create the room.
+			httpCreateRoom(w, r, session.Values["ID"].(string))
+		case "DELETE": // DELETE attempts to delete the room if the user is the admin.
+			httpDestroyRoom(w, r, session.Values["ID"].(string), r.PathValue("rid"))
+		case "GET": // GET attempts to join the room.
+			httpJoinRoom(w, r, session.Values["ID"].(string), r.PathValue("rid"))
+		case "PUT": // PUT attempts to change the state of the game from the admin.
+			w.Write([]byte("PUT /rooms/{rid}"))
+		}
+	})
+
+	http.HandleFunc("/users/{uid}", func(w http.ResponseWriter, r *http.Request) {
+		session, err := track(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		switch r.Method {
+		case "POST": // POST attempts to set the session user's dial.
+			httpCreateUser(w, r, session.Values["ID"].(string))
+		case "GET": // GET attempts to request the given user's dial.
+			httpGetUser(w, r, r.PathValue("uid"))
+		}
+	})
+
+	http.ListenAndServe(":9000", nil)
+}
